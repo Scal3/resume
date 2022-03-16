4ba30d13c66db5c8264b7d8dcfdcc8a1646d6ad8
scal3 <cool.goga501@gmail.com>
2022-03-16 23:05:04 +0300

    feat/create MyTechsSkills component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import Header from '../Header/Header';
 import AboutMe from '../AboutMe/AboutMe';
+import MyTechsSkills from '../MyTechsSkills/MyTechsSkills';
 
 const App = () => {
   return (
     <div className="App">
       <Header/>
       <AboutMe/>
+      <MyTechsSkills/>
     </div>
   );
 }
