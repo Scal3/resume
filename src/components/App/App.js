cd3d10bb57c0b5d1d25dffe407e72211d4d79ee3
scal3 <cool.goga501@gmail.com>
2022-03-23 17:49:12 +0300

    feat/create WorkExperience component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import Header from '../Header/Header';
 import AboutMe from '../AboutMe/AboutMe';
 import MyTechsSkills from '../MyTechsSkills/MyTechsSkills';
 import MyProjects from '../MyProjects/MyProjects';
+import WorkExperience from '../WorkExperience/WorkExperience';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <AboutMe/>
       <MyTechsSkills/>
       <MyProjects/>
+      <WorkExperience/>
     </div>
   );
 }
